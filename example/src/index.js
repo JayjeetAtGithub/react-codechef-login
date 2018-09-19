962027dc3553fe7fb1979e5560b58b6bf68b5d24
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-09-19 22:40:43 +0530

    Initial commit

--- example/src/index.js
+++ example/src/index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018 Jayjeet Chakraborty.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
